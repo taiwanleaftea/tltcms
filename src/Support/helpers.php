80bc8f12c40877cd4f82d3f37aa056ae706dfbb0
Andrey Mamykin <amamykin@gmail.com>
2024-12-10 12:35:53 +0100

    0.3.4
    image provider bugfix

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -5,7 +5,7 @@ use Tltcms\Support\Facades\MainMenu;
 use Tltcms\Support\Facades\Settings;
 use Jaybizzle\CrawlerDetect\CrawlerDetect;
 
-const VERSION = '0.3.3';
+const VERSION = '0.3.4';
 
 if (! function_exists('settings')) {
     function settings($key, $default = null) {
